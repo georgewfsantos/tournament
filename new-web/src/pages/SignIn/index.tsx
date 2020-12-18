eb2c5fdf3281de6359b449d40b85d606d490f91b
gweverton21@hotmail.com <gweverton21@hotmail.com>
2020-12-18 13:25:19 -0300

    write validation schema for signIn form using yup

--- new-web/src/pages/SignIn/index.tsx
+++ new-web/src/pages/SignIn/index.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback } from 'react';
+import * as Yup from 'yup';
 import {
   FiUser,
   FiPhone,
@@ -14,8 +15,23 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
-  const handleSubmit = useCallback((data: object): void => {
-    console.log(data);
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome é obrigatório'),
+        email: Yup.string().email('Digite um formato válido de e-mail'),
+        category_id: Yup.number().required(),
+        phone_number: Yup.string().required(),
+        guests: Yup.string().min(6, 'Digite o Nome Completo do Acompanhante'),
+        restrictions: Yup.string(),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }, []);
 
   return (
