ed4c9c398d81bfb5d01bba5b3f20846673dff4de
gweverton21@hotmail.com <gweverton21@hotmail.com>
2020-12-21 13:37:47 -0300

    fix error in Subscriptions and SignIn page

--- new-web/src/pages/SignIn/index.tsx
+++ new-web/src/pages/SignIn/index.tsx
@@ -1,13 +1,6 @@
 import React, { useCallback, useRef } from 'react';
 import * as Yup from 'yup';
-import {
-  FiUser,
-  FiPhone,
-  FiList,
-  FiUsers,
-  FiEdit,
-  FiMail,
-} from 'react-icons/fi';
+import { FiLock, FiMail } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import getValidationErrors from '../../utils/getValidationErrors';
@@ -25,15 +18,8 @@ const SignIn: React.FC = () => {
       formRef.current?.setErrors({});
 
       const schema = Yup.object().shape({
-        name: Yup.string().required('Nome é obrigatório'),
         email: Yup.string().email('Digite um formato válido de e-mail'),
-        category_id: Yup.number()
-          .required()
-          .moreThan(0, 'Deve ser um número de 1 a 5')
-          .lessThan(6, 'Deve ser um número de 1 a 5'),
-        phone_number: Yup.string().required('Número de telefone é obrigatório'),
-        guests: Yup.string(),
-        restrictions: Yup.string(),
+        password: Yup.number().required().max(10),
       });
 
       await schema.validate(data, {
@@ -55,24 +41,15 @@ const SignIn: React.FC = () => {
           onSubmit={handleSubmit}
           initialData={{ name: 'Diego' }}
         >
-          <h1>Faça sua inscrição</h1>
+          <h1>Acessar como Admin</h1>
 
-          <Input name="name" icon={FiUser} placeholder="Nome Completo" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
           <Input
-            type="number"
-            name="category_id"
-            icon={FiList}
-            placeholder="Classe"
-            defaultValue={0}
+            type="password"
+            name="password"
+            icon={FiLock}
+            placeholder="Senha"
           />
-          <Input name="phone_number" icon={FiPhone} placeholder="Telefone" />
-          <Input
-            name="guests"
-            icon={FiUsers}
-            placeholder="Acompanhantes (se houver)"
-          />
-          <Input name="restrictions" icon={FiEdit} placeholder="Mensagem" />
 
           <Button type="submit">Entrar</Button>
         </Form>
--- new-web/src/pages/Subscriptions/index.tsx
+++ new-web/src/pages/Subscriptions/index.tsx
@@ -1,6 +1,13 @@
 import React, { useCallback, useRef } from 'react';
 import * as Yup from 'yup';
-import { FiLock, FiMail } from 'react-icons/fi';
+import {
+  FiUser,
+  FiPhone,
+  FiList,
+  FiUsers,
+  FiEdit,
+  FiMail,
+} from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import getValidationErrors from '../../utils/getValidationErrors';
@@ -18,8 +25,15 @@ const Subscriptions: React.FC = () => {
       formRef.current?.setErrors({});
 
       const schema = Yup.object().shape({
+        name: Yup.string().required('Nome é obrigatório'),
         email: Yup.string().email('Digite um formato válido de e-mail'),
-        password: Yup.number().required().max(10),
+        category_id: Yup.number()
+          .required()
+          .moreThan(0, 'Deve ser um número de 1 a 5')
+          .lessThan(6, 'Deve ser um número de 1 a 5'),
+        phone_number: Yup.string().required('Número de telefone é obrigatório'),
+        guests: Yup.string(),
+        restrictions: Yup.string(),
       });
 
       await schema.validate(data, {
@@ -41,15 +55,24 @@ const Subscriptions: React.FC = () => {
           onSubmit={handleSubmit}
           initialData={{ name: 'Diego' }}
         >
-          <h1>Acessar como Admin</h1>
+          <h1>Faça sua inscrição</h1>
 
+          <Input name="name" icon={FiUser} placeholder="Nome Completo" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
           <Input
-            type="password"
-            name="password"
-            icon={FiLock}
-            placeholder="Senha"
+            type="number"
+            name="category_id"
+            icon={FiList}
+            placeholder="Classe"
+            defaultValue={0}
           />
+          <Input name="phone_number" icon={FiPhone} placeholder="Telefone" />
+          <Input
+            name="guests"
+            icon={FiUsers}
+            placeholder="Acompanhantes (se houver)"
+          />
+          <Input name="restrictions" icon={FiEdit} placeholder="Mensagem" />
 
           <Button type="submit">Entrar</Button>
         </Form>
