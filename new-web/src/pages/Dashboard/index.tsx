147ae84dd91b89af587ec7d03d944d94b487c4df
gweverton21@hotmail.com <gweverton21@hotmail.com>
2020-12-31 14:54:14 -0300

    remove test link to Bracket Display from Dashboard page after testing it

--- new-web/src/pages/Dashboard/index.tsx
+++ new-web/src/pages/Dashboard/index.tsx
@@ -7,7 +7,6 @@ const Dashboard: React.FC = () => {
   return (
     <Container>
       <Link to="/brackets">Brackets</Link>
-      <Link to="/brackets/bracketDisplay">Brackets Display</Link>
     </Container>
   );
 };
