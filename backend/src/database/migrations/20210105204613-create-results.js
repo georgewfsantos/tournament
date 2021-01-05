57dad0720e19378c6c7a2c9ba0858d09b2c5d6e1
gweverton21@hotmail.com <gweverton21@hotmail.com>
2021-01-05 18:00:47 -0300

    create and run the results migration

---
+++ backend/src/database/migrations/20210105204613-create-results.js
@@ -0,0 +1,68 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('results', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      match_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      classe: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      player_1: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      player_1_score_1: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      player_1_score_2: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      player_1_score_3: {
+        type: Sequelize.INTEGER,
+      },
+      player_2: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      player_2_score_1: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      player_2_score_2: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      player_2_score_3: {
+        type: Sequelize.INTEGER,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('results');
+  },
+};
