28fc57fd956a5774f57e3277fb3a78a9935d1bf2
gweverton21@hotmail.com <gweverton21@hotmail.com>
2021-01-09 14:26:02 -0300

    change route that fetches category name from api

--- new-web/src/pages/SubscriptionView/SubscriptionList/index.tsx
+++ new-web/src/pages/SubscriptionView/SubscriptionList/index.tsx
@@ -7,11 +7,16 @@ import { BracketItemParams } from '../../Brackets/BracketItem';
 
 import { Container, Content, ListRow } from './styles';
 
+interface Category {
+  id: number;
+  name: string;
+}
+
 interface Player {
   id: number;
   name: string;
   email: string;
-  category_id: number;
+  category_id: string;
   phone_number: string;
   guests: string;
   restrictions: string;
@@ -20,40 +25,42 @@ interface Player {
 
 const SubscriptionList: React.FC = () => {
   const { categoryId } = useParams<BracketItemParams>();
+  const [category, setCategory] = useState({} as Category);
   const [players, setPlayers] = useState<Player[]>([]);
 
   useEffect(() => {
     async function loadPlayers() {
-      const response = await api.get(`/players/${categoryId}`);
+      const response = await api.get(`/categories/${categoryId}`);
+      const playersResponse = await api.get(`/players/${categoryId}`);
 
-      setPlayers(response.data);
+      setCategory(response.data);
+      setPlayers(playersResponse.data);
     }
 
     loadPlayers();
   }, [categoryId]);
 
   return (
-    <>
-      <Container>
-        <Content>
-          <ListRow id="list-header">
-            <p>Nome do jogador</p>
-            <p>Telefone</p>
-            <p>Data de inscrição</p>
+    <Container>
+      <h1>{category?.name}</h1>
+      <Content>
+        <ListRow id="list-header">
+          <p>Nome do jogador</p>
+          <p>Telefone</p>
+          <p>Data de inscrição</p>
+          <p>Outra opção...</p>
+        </ListRow>
+
+        {players?.map(player => (
+          <ListRow key={player.id}>
+            <p>{player.name}</p>
+            <p>{player.phone_number}</p>
+            <p>{format(new Date(player.createdAt), 'dd/MM/yyyy')}</p>
             <p>Outra opção...</p>
           </ListRow>
-
-          {players?.map(player => (
-            <ListRow key={player.id}>
-              <p>{player.name}</p>
-              <p>{player.phone_number}</p>
-              <p>{format(new Date(player.createdAt), 'dd/MM/yyyy')}</p>
-              <p>Outra opção...</p>
-            </ListRow>
-          ))}
-        </Content>
-      </Container>
-    </>
+        ))}
+      </Content>
+    </Container>
   );
 };
 
--- new-web/src/pages/SubscriptionView/SubscriptionList/styles.ts
+++ new-web/src/pages/SubscriptionView/SubscriptionList/styles.ts
@@ -2,8 +2,14 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  h1 {
+    margin-top: 10px;
+    text-transform: capitalize;
+  }
 `;
 
 export const Content = styled.div`
